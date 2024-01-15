e84a877b6309959839fbc917f17618bebac17125
Raisul Islam Hridoy <raisul15-3813@diu.edu.bd>
2024-01-16 00:50:05 +0600

    modify error page

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,17 @@
-
+import { Link } from "react-router-dom";
+import { IoArrowBackCircle } from "react-icons/io5";
 
 const ErrorPage = () => {
    return (
-      <div>
-         <h2 className="text-xl text-center">Something Error</h2>
+      <div className="my-3 md:my-5 lg:my-8">
+         <Link to="/">
+            <div className="flex justify-center items-center">
+               <IoArrowBackCircle className="text-2xl mr-1"></IoArrowBackCircle>
+               <h2 className="text-xl">Back to Home</h2>
+            </div></Link>
+         <div className="flex justify-center">
+            <img src="https://i.ibb.co/BfDBDHg/error.gif" alt="" className="my-5 rounded-3xl shadow-xl" />
+         </div>
       </div>
    );
 };
