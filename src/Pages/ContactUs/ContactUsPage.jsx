36d270a220bd93320fb5eebe2c9b525381208667
Raisul Islam Hridoy <raisul15-3813@diu.edu.bd>
2024-01-18 00:54:00 +0600

    [Error Page]:modify code

---
+++ src/Pages/ContactUs/ContactUsPage.jsx
@@ -0,0 +1,11 @@
+
+
+const ContactUsPage = () => {
+   return (
+      <div>
+         <h2 className="text-xl text-center font-bold">Contact Us</h2>
+      </div>
+   );
+};
+
+export default ContactUsPage;
