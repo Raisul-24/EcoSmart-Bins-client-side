a231648f35360f3572b0caa588211e369f2e199b
Raisul Islam Hridoy <raisul15-3813@diu.edu.bd>
2024-01-25 15:32:09 +0600

    [Login/Registration]:authenticate user by firebase

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,39 @@
 /* eslint-disable react/no-unescaped-entities */
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import SocialLogin from "../../SharedComponents/SocialLogin/SocialLogin";
-
+import { useForm } from "react-hook-form";
+import UseAuth from "../../Hooks/UseAuth";
+import { toast } from "react-hot-toast";
 
 const Login = () => {
+   const { register, handleSubmit, formState: { errors }, } = useForm();
+   const { signIn } = UseAuth();
+
+   const navigate = useNavigate();
+   const location = useLocation();
+
+   const from = location.state?.from?.pathname || "/";
+   // console.log('state in the location login page', location.state)
+
+   const onSubmit = async (data) => {
+      // console.log(data);
+      // console.log(errors)
+      const toastId = toast.loading('Logging In....');
+      try {
+         await signIn(data.email, data.password)
+         toast.success('LogIn Successfully!!', { id: toastId })
+         
+         if (location.state && location.state.from) {
+            navigate(location.state.from.pathname)
+         }
+         else {
+            navigate(from, { replace: true });
+         }
+      } catch (error) {
+         toast.error(error.message, { id: toastId })
+      }
+   }
+  
     return (
         <div className="flex flex-col md:flex-row h-full py-16 md:py-40 lg:py-32 items-center bg-[url(https://i.ibb.co/17R85wC/authentication.png)]">
          <div className="w-1/2 flex justify-center md:justify-end md:mx-3">
@@ -16,20 +46,24 @@ const Login = () => {
                      LogIn Now!!
                   </h3>
                </div>
-               <form>
+               <form onSubmit={handleSubmit(onSubmit)}>
                   <div className="flex flex-col gap-8 p-6">
                      <div className="relative h-11 w-full min-w-[200px]">
                         <input type="email"
+                        {...register("email", { required: true })}
                            className="w-full h-full px-3 py-3  text-sm font-normal transition-all bg-transparent border rounded-md peer border-blue-gray-200 border-t-transparent text-blue-gray-700 outline outline-0 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-green-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                        required/>
+                        />
+                        {errors.email && <span className="text-red-600 text-xs font-medium">Email is required</span>}
                         <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-green-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-green-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-green-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                            Email
                         </label>
                      </div>
                      <div className="relative h-11 w-full min-w-[200px]">
                         <input type="password"
+                         {...register("password", { required: true })}
                            className="w-full h-full px-3 py-3  text-sm font-normal transition-all bg-transparent border rounded-md peer border-blue-gray-200 border-t-transparent text-blue-gray-700 outline outline-0 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-green-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                        required/>
+                        />
+                         {errors.password && <span className="text-red-600 text-xs font-medium">Password is required</span>}
                         <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-green-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-green-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-green-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                            Password
                         </label>
--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Router";
 import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider from "./Provider/AuthProvider";
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+    <AuthProvider>
     <RouterProvider router={router} />
     <Toaster />
+    </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
