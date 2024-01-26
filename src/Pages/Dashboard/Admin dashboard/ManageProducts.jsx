cff715e7ee77341b548088276468cb8f281479f0
MD SAMIT INTESAR <saadsamit64@gmail.com>
2024-01-26 16:26:08 +0600

    fix some bugs on manage service page

--- src/Pages/Dashboard/Admin dashboard/ManageProducts.jsx
+++ src/Pages/Dashboard/Admin dashboard/ManageProducts.jsx
@@ -39,59 +39,70 @@ const ManageProducts = () => {
       <div className="border-b-2">
         <h2 className="text-4xl mb-5 text-center ">Manage Shop Data</h2>
       </div>
-      <div className="overflow-x-auto">
-        <table className="table w-full">
-          {/* head */}
-          <thead className="text-center bg-brand-color ">
-            <tr>
-              <th>#</th>
-              <th>Image</th>
-              <th>Products Name</th>
-              <th>Price</th>
-              <th>Details</th>
-              <th>Update</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
-          <tbody className="text-center font-medium">
-            {products?.map((item, index) => (
-              <tr key={item._id}>
-                <td>{index + 1}</td>
-                <td>
-                  <div className="flex items-center gap-3">
-                    <div className="avatar">
-                      <div className="mask mask-decagon  w-12 h-12">
-                        <img
-                          src={item.img}
-                          alt="Avatar Tailwind CSS Component"
-                        />
+      {loading ? (
+        <div className="text-center mt-20">
+          <span className="loading bg-[#3A9E1E] loading-spinner loading-lg"></span>
+        </div>
+      ) : (
+        <div className="overflow-x-auto">
+          <table className="table w-full">
+            {/* head */}
+            <thead className="text-center bg-brand-color ">
+              <tr>
+                <th>#</th>
+                <th>Image</th>
+                <th>Products Name</th>
+                <th>Price</th>
+                <th>Details</th>
+                <th>Update</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody className="text-center font-medium">
+              {products?.map((item, index) => (
+                <tr key={item._id}>
+                  <td>{index + 1}</td>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar">
+                        <div className="mask mask-decagon  w-12 h-12">
+                          <img
+                            src={item.img}
+                            alt="Avatar Tailwind CSS Component"
+                          />
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </td>
-                <td>{item.title}</td>
-                <td>$ {item.price}</td>
-                <td className="text-right">
-                  <button className="btn btn-ghost ">See Details</button>
-                </td>
-                <td>
-                  <button className="btn btn-sm rounded-none bg-gradient-to-r from-brand-color to-green-300 hover:bg-gradient-to-r hover:from-green-300 hover:to-brand-color  text-white">
-                    Update
-                  </button>
-                </td>
-                <td>
-                  <button
-                    onClick={() => handleDeleteItem(item)}
-                    className="btn rounded-none btn-sm bg-red-600 text-white"
-                  >
-                    <FaTrash></FaTrash>
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+                  </td>
+                  <td>{item.title}</td>
+                  <td>$ {item.price}</td>
+                  <td className="text-right">
+                    <Link
+                      to={`/shop/${item._id}`}
+                      className="btn btn-ghost hover:text-brand-color"
+                    >
+                      See Details
+                    </Link>
+                  </td>
+                  <td>
+                    <button className="btn btn-sm bg-gradient-to-r from-brand-color to-green-500 hover:bg-gradient-to-r hover:from-green-500 hover:to-brand-color  text-white">
+                      Update
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => handleDeleteItem(item)}
+                      className="btn rounded-lg btn-sm bg-red-600 hover:bg-red-700 text-white"
+                    >
+                      <FaTrash></FaTrash>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
