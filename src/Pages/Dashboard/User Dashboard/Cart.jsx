8c2c53bc7c633576153052c00ce2ffa8e586425c
Coder Mahdi <codermahdihasan@gmail.com>
2024-01-29 22:52:23 +0600

    cart component added

--- src/Pages/Dashboard/User Dashboard/Cart.jsx
+++ src/Pages/Dashboard/User Dashboard/Cart.jsx
@@ -1,11 +1,19 @@
-
+import { useSelector } from "react-redux";
 
 const Cart = () => {
-    return (
-        <div>
-            my cart
-        </div>
-    );
-};
+  const cartItems = useSelector((state) => state.cart.items);
 
+  return (
+    <div>
+      <h2>Shopping Cart</h2>
+      <ul>
+        {cartItems.map((item) => (
+          <li key={item._id}>
+            {item.name} - ${item.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 export default Cart;
