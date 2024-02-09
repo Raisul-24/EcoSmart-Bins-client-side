1dd74be0e4a8d23779d00ffb9e242af2143a6021
Coder Mahdi <codermahdihasan@gmail.com>
2024-02-09 10:06:24 +0600

    trying to design improve of blog

--- src/Pages/Blogs/BlogCard.jsx
+++ src/Pages/Blogs/BlogCard.jsx
@@ -34,7 +34,7 @@ const BlogCard = ({ blog }) => {
   return (
     <>
       <div
-        className="bg-white border flex flex-col h-full  p-4 border-white rounded-lg 
+        className="bg-white border flex flex-col h-full mb-12 p-4 border-white rounded-lg 
 	 shadow-2xl pb-4"
       >
         <div className="">
--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -1,15 +1,16 @@
 import BlogCard from "./BlogCard";
 import useGetBlog from "../../API/BlogApi/useGetBlog";
+import { FaSearch } from "react-icons/fa";
 
 const Blog = () => {
   const [blogs, loading] = useGetBlog();
 
   return (
-    <div className="py-20">
+    <div className="py-20 mx-8">
       <h5 className="text-2xl text-green-600 font-semibold text-center pb-5">
         Our Articles
       </h5>
-      <h3 className="text-5xl font-bold text-[#000000] text-center">
+      <h3 className="text-5xl font-bold text-[#000000] text-center mb-20">
         Get more updates from Eco-Smart Bins
       </h3>
       {loading ? (
@@ -17,10 +18,24 @@ const Blog = () => {
           <span className="loading bg-[#3A9E1E] loading-spinner loading-lg"></span>
         </div>
       ) : (
-        <div className="grid md:grid-cols-2 grid-cols-1 lg:grid-cols-3 gap-6 pt-8">
-          {blogs?.map((blog) => (
-            <BlogCard key={blog?._id} blog={blog}></BlogCard>
-          ))}
+        <div className="md:grid grid-cols-9 gap-12 items-start justify-center">
+          <div className="col-span-6 mb-5 lg:grid-cols-3 ">
+            {blogs?.map((blog) => (
+              <BlogCard key={blog?._id} blog={blog}></BlogCard>
+            ))}
+          </div>
+          <div>
+            <div className="flex col-span-3 w-96 p-10 bg-[#ebf5e9] mt-4">
+              <input
+                className="h-12 w-80 relative p-4"
+                placeholder="Search..."
+                label="Input With Icon"
+              />
+              <span className="absolute top-0 bottom-0 right-0 left-0 -ml-[10px] mt-[435px] text-gray-400 text-xl">
+                <FaSearch />
+              </span>
+            </div>
+          </div>
         </div>
       )}
     </div>
