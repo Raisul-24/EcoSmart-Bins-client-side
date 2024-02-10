e8993ae2ad602bf25225083476afc91a075b87f7
Coder Mahdi <codermahdihasan@gmail.com>
2024-02-10 23:53:22 +0600

    add chat kit

--- src/LayOut/MainLayOut.jsx
+++ src/LayOut/MainLayOut.jsx
@@ -2,16 +2,14 @@ import { Outlet } from "react-router-dom";
 import Footer from "../SharedComponents/Footer";
 import Navbar from "../SharedComponents/Navbar/Navbar";
 
-
-
 const MainLayOut = () => {
-   return (
-      <div className="max-w-screen-2xl mx-auto">
-         <Navbar></Navbar>
-         <Outlet></Outlet>
-        <Footer></Footer>
-      </div>
-   );
+  return (
+    <div className="max-w-screen-2xl mx-auto font-montserrat">
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default MainLayOut;
---
+++ src/Pages/ChatBot/CoBot.jsx
@@ -0,0 +1,16 @@
+import BotAvatar from "../../assets/images/icon2.png";
+
+const CoBotAvatar = () => {
+  return (
+    <div className="react-chatbot-kit-chat-bot-avatar">
+      <div
+        className="react-chatbot-kit-chat-bot-avatar-container"
+        style={{ background: "none" }}
+      >
+        <img alt="BotAvatar" src={BotAvatar} />
+      </div>
+    </div>
+  );
+};
+
+export default CoBotAvatar;
