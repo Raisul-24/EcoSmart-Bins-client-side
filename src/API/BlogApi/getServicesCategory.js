df56629897d5d2f3e6bb8300c71432a563babc4e
Coder Mahdi <codermahdihasan@gmail.com>
2024-02-16 19:33:54 +0600

    modifying service page

---
+++ src/API/BlogApi/getServicesCategory.js
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "../../axios/axiosPublic";
+
+const getServicesCategories = (id) => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const axios = useAxiosPublic();
+  const servicesData = async () => {
+    const { data } = await axios.get(`/services-category`);
+    console.log(data);
+    return data;
+  };
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { data, isPending, refetch } = useQuery({
+    queryKey: ["getCategory", id],
+    queryFn: servicesData,
+  });
+  return [data, isPending, refetch];
+};
+
+export default getServicesCategories;
