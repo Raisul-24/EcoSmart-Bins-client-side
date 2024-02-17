6f66a67da11c2d80a45da2389bee917da8c98f2e
Coder Mahdi <codermahdihasan@gmail.com>
2024-02-17 23:15:44 +0600

    PieChart added on serviceDetails

---
+++ src/Pages/ServiceDetail/ServiceDetailsFAQ.jsx
@@ -0,0 +1,50 @@
+const ServiceDetailsFAQ = () => {
+  return (
+    <div>
+      <div className="collapse collapse-plus rounded-lg pb-2 text-[#101a30] hover:text-brand-color">
+        <input type="checkbox" name="accordion" />
+        <div className="collapse-title  text-xl font-bold">
+          <p>Food Recycling And Anaerobic Digestion</p>
+        </div>
+        <div className="collapse-content">
+          <p>
+            Recycling Facilities essential in providing quality raw materials to
+            the production industry, as they designed to separate recyclables
+            into their individual material streams and prepare them for sale
+          </p>
+        </div>
+      </div>
+      <p className="border-b-2" />
+      <div className="collapse  collapse-plus rounded-lg pb-2 text-[#101a30] hover:text-brand-color">
+        <input type="checkbox" name="accordion" />
+        <div className="collapse-title  text-xl font-bold">
+          <p>Mechanical And Biological Treatment</p>
+        </div>
+        <div className="collapse-content">
+          <p>
+            Recycling Facilities essential in providing quality raw materials to
+            the production industry, as they designed to separate recyclables
+            into their individual material streams and prepare them for sale
+          </p>
+        </div>
+      </div>
+      <p className="border-b-2" />
+      <div className="collapse  collapse-plus rounded-lg pb-2 text-[#101a30] hover:text-brand-color">
+        <input type="checkbox" name="accordion" />
+        <div className="collapse-title  text-xl font-bold">
+          <p>Metal And Plastic Recycling Facilities</p>
+        </div>
+        <div className="collapse-content">
+          <p>
+            Recycling Facilities essential in providing quality raw materials to
+            the production industry, as they designed to separate recyclables
+            into their individual material streams and prepare them for sale
+          </p>
+        </div>
+      </div>
+      <p className="border-b-2" />
+    </div>
+  );
+};
+
+export default ServiceDetailsFAQ;
