b5d52bfd718b32f35884ee9251d741c996ac036b
Coder Mahdi <codermahdihasan@gmail.com>
2024-02-19 16:54:25 +0600

    charts on service

--- src/SharedComponents/Navbar/ServiceNavbar.jsx
+++ src/SharedComponents/Navbar/ServiceNavbar.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 const ServiceNavbar = ({ data }) => {
-  console.log(data);
+//  console.log(data);
   return (
     <div className="overflow-hidden">
       <motion.li
