1ef02d342baaf69aee5adea435e9d42e953e06f1
muna <muna3828@gmail.com>
2024-02-21 16:43:02 +0600

    solved responsive issue from all page banner

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -27,7 +27,7 @@ const About = () => {
       >
         <div className="hero-overlay bg-black bg-opacity-60 absolute inset-0"></div>
         <motion.div
-          className="bottom-10 left-20 absolute "
+          className="lg:bottom-10 lg:left-20 bottom-5 left-5 absolute hidden md:block "
           animate={{
             scale: [1, 1.2, 1],
           }}
@@ -41,7 +41,7 @@ const About = () => {
           <motion.img
             src={img1}
             alt="Zooming Image"
-            className="w-32"
+            className="lg:w-32 w-24"
           />
         </motion.div>
         <div className="hero-content text-center text-neutral-content relative z-10">
@@ -53,7 +53,7 @@ const About = () => {
           </div>
         </div>
         <motion.div
-          className="absolute top-20 right-40 "
+          className="absolute top-5 right-5 lg:top-20 lg:right-40 hidden md:block"
           animate={{
             rotate: 360,
           }}
--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -39,7 +39,7 @@ const Blog = () => {
           >
             <div className="hero-overlay bg-black bg-opacity-60 absolute inset-0"></div>
             <motion.div
-          className="bottom-10 left-20 absolute "
+          className="lg:bottom-10 lg:left-20 bottom-5 left-5 absolute hidden md:block "
           animate={{
             scale: [1, 1.2, 1],
           }}
@@ -53,7 +53,7 @@ const Blog = () => {
           <motion.img
             src={img1}
             alt="Zooming Image"
-            className="w-32"
+            className="w-24 lg:w-32"
           />
         </motion.div>
             <div className="hero-content text-center text-neutral-content relative z-10">
@@ -65,7 +65,7 @@ const Blog = () => {
               </div>
             </div>
             <motion.div
-          className="absolute top-20 right-40 "
+          className="absolute top-5 right-5 lg:top-20 lg:right-40 hidden md:block"
           animate={{
             rotate: 360,
           }}
