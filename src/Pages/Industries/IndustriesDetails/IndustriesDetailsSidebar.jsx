110a699cdb2a2074cd32d7741d4851460dce1e82
Coder Mahdi <codermahdihasan@gmail.com>
2024-02-25 18:27:58 +0600

    modifyed

--- src/Pages/Industries/IndustriesDetails/IndustriesDetailsSidebar.jsx
+++ src/Pages/Industries/IndustriesDetails/IndustriesDetailsSidebar.jsx
@@ -3,46 +3,28 @@ import {
   FaFacebookMessenger,
   FaPhoneAlt,
 } from "react-icons/fa";
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import sidebarBg from "../../../assets/images/sideber-of-service-details.webp";
 import getIndustriesCategory from "../../../API/IndustriesApi/getIndustriesCategory";
-import getAIndustries from "../../../API/IndustriesApi/getAIndustries";
 
 const IndustriesDetailsSidebar = () => {
-  const { id } = useParams();
   const [category, isPending] = getIndustriesCategory();
-  const [data, dataLoading] = getAIndustries(id);
-  //  console.log(data?._id);
 
   if (isPending) {
     return;
   }
-  if (dataLoading) {
-    return (
-      <div className="text-center py-20">
-        <span className="loading bg-[#3A9E1E] loading-spinner loading-lg"></span>
-      </div>
-    );
-  }
+
   return (
     <>
       {/* part 1 categories buttons */}
       <div className="rounded-xl p-10 bg-[#e9f1ea] mt-4">
-        <p className="text-xl font-extrabold py-5">Our Industries</p>
+        <p className="text-xl font-extrabold py-5 mb-5">Our Industries</p>
         {category?.map((category) => (
-          <button key={category?._id}>
-            <Link
-              to={`/industries/${data?._id}`}
-              className="h-16 px-8 bg-[#182822] hover:bg-[#257830]
-						rounded-md text-start w-72 flex justify-start items-center
-						gap-4 text-white text-md font-bold mb-6"
-            >
-              <span className="text-2xl">
-                <FaArrowCircleRight />
-              </span>
+          <ul className=" " key={category?._id}>
+            <li className="h-12 w-72 pl-12 font-bold">
               {category?.industriesCategory}
-            </Link>
-          </button>
+            </li>
+          </ul>
         ))}
       </div>
       {/* part 2 content with get start button */}
