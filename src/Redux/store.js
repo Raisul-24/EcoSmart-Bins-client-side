50438daf34e5168a2877e425cbcbfe02af719a16
muna <muna3828@gmail.com>
2024-03-04 23:15:38 +0600

    [career]: added job post card

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartsReducer from "./cartSlice";
 import ServiceSlice from "./ServiceSlice";
 import getApi from "./userApi/getApi";
+import careerSlice from "./CareerSlice";
 
 const store = configureStore({
   reducer: {
     carts: cartsReducer,
     services: ServiceSlice,
+    career: careerSlice,
     [getApi.reducerPath]: getApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
