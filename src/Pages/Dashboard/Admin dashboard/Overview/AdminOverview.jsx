9c4343c0d1b1a363ed7a5de5709da5a34ec36b90
MD SAMIT INTESAR <saadsamit64@gmail.com>
2024-03-07 17:43:09 +0600

    worker over work

---
+++ src/Pages/Dashboard/Admin dashboard/Overview/AdminOverview.jsx
@@ -0,0 +1,46 @@
+import { useGetApiQuery } from "../../../../Redux/userApi/getApi";
+import Linechart from "./LineChart";
+import OverviewItem from "./OverviewItem";
+
+const AdminOverview = () => {
+  const { data, isLoading } = useGetApiQuery("/services");
+  const { data: overViewData, isLoading: loading } =
+    useGetApiQuery("/adminOverview");
+  if (isLoading || loading) {
+    return (
+      <div className="text-center">
+        <span className="loading bg-[#3A9E1E] loading-spinner loading-lg"></span>
+      </div>
+    );
+  }
+  return (
+    <div className="mt-16">
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
+        {overViewData?.map((item, idx) => (
+          <OverviewItem data={item} key={idx} />
+        ))}
+        <Linechart />
+        <div className="lg:col-span-1 md:col-span-2 col-span-1">
+          <div className="border border-brand-color flex flex-col p-5 rounded-xl">
+            <h3 className="text-2xl font-bold capitalize text-center mb-5">
+              our services
+            </h3>
+            {data?.map((item, idx) => (
+              <p
+                key={item?._id}
+                className="text-lg flex gap-4 pb-1 font-medium"
+              >
+                <span className="text-xl block text-brand-color font-bold">
+                  {++idx}.
+                </span>{" "}
+                {item?.title}
+              </p>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminOverview;
